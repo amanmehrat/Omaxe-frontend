cbb65cfd4979830ed17839c06829e4f5c4488f53
Aman Mehrat <aman.mehrat956@gmail.com>
2021-03-10 23:42:40 +0530

    production irl change

--- src/config.js
+++ src/config.js
@@ -11,7 +11,7 @@ const staging = {
 };
 
 const prod = {
-    restApiBase: 'http://localhost:3000',
+    restApiBase: 'https://api.techencode.in',
     graphQLApi: 'https://graph.techencode.in/v1/graphql',
     sentryLogs: 'https://fe07833aa39a4b59a908529720162e72@o547107.ingest.sentry.io/5669337'
 };
