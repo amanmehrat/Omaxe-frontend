b96a4442e137cb0138587e88597d946fca236f89
Aman Mehrat <aman.mehrat956@gmail.com>
2021-04-13 21:53:42 +0530

    Give Print Bills options on Property Detail Page

--- src/components/grid/ViewHistoryGrid.js
+++ src/components/grid/ViewHistoryGrid.js
@@ -1,13 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import CamByIdModels from './colmodels/CamByIdModels';
 import ElecByIdModels from './colmodels/ElecByIdModels';
 import Table from './table/Table';
+import IndeterminateCheckbox from './table/IndeterminateCheckbox';
 
 
-const ViewHistoryGrid = ({ bills, billType }) => {
+const ViewHistoryGrid = ({ bills, billType, setBills }) => {
     let models = billType == 1 ? CamByIdModels : ElecByIdModels;
-
-    const columns = React.useMemo(() => [...models()], []);
+    const columns = React.useMemo(
+        () => [
+            {
+                id: "selection",
+                Header: ({ getToggleAllRowsSelectedProps }) => (
+                    <div>
+                        <IndeterminateCheckbox {...getToggleAllRowsSelectedProps()} />
+                    </div>
+                ),
+                Cell: ({ row }) => (
+                    <div>
+                        <IndeterminateCheckbox {...row.getToggleRowSelectedProps()} />
+                    </div>
+                )
+            },
+            ...models()
+        ],
+        []
+    );
     const data = React.useMemo(() => bills, [bills]);
 
     return (
@@ -15,6 +33,7 @@ const ViewHistoryGrid = ({ bills, billType }) => {
             <Table
                 columns={columns}
                 data={data}
+                onRowSelect={rows => setBills(rows)}
                 hiddenColumns={[]}
             />
         </>
